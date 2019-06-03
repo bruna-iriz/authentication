0cdbfad14b56c8f7b396e0028a768de4c923ea1e
bruna-iriz <bruna.iriz@hotmail.com>
2019-06-02 22:54:38 -0300

    atualizando WebSecurityConfig

---
+++ src/main/java/br/com/authentication/api/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package br.com.authentication.api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests()
+		.antMatchers("HttpMethod.GET", "/phones").permitAll()
+		.antMatchers("HttpMethod.GET", "/users").hasRole("ADMIN")
+		.and().formLogin();
+	}
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("admin")
+		.password("adm1n")
+		.roles("ADMIN");
+	}
+}
